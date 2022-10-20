23f6255ad80db21e369bfa5364546795ccaac280
KCC New Media Arts <gargiulo@hawaii.edu>
2022-10-20 11:55:14 -1000

    set up scroll triggered animation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // OPEN AND CLOSE SITE NAV MENU
     const mysitenav = document.querySelector('.site-nav');
     const mymenubutton = document.querySelector('.menu-button');
-
     mymenubutton.onclick = function () {
         if (mysitenav.getAttribute('data-navstate') === 'closed') {
             mysitenav.setAttribute('data-navstate','open');
@@ -11,5 +10,20 @@ document.addEventListener('DOMContentLoaded', function () {
             mysitenav.setAttribute('data-navstate','closed');
         };
     }; 
+
+    // SCROLL TRIGGERED ANIMATION
+    const myobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute("data-sectionstate", "active");
+        } else {
+            entry.target.setAttribute("data-sectionstate", "inactive");
+        }
+        });
+    });
+  
+    document.querySelectorAll("section").forEach((section) => {
+        myobserver.observe(section);
+    });
     
 });
